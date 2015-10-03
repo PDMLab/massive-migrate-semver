4d1760e1ae4487548705ec3b0f11e0d86eb16773
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2015-10-03 17:49:58 +0200

    Refactor migrations to require options only once.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var should = require('chai').should();
 var massive = require('massive');
 var conn = "postgresql://postgres:postgres@localhost:5432/postgres";
-var semverMigration = require('../index');
+var massiveMigrateSemVer = require('../index');
 
 
 describe('When running migration from no tables to 0.1.0 with no pgmiration table existing', function () {
@@ -9,7 +9,7 @@ describe('When running migration from no tables to 0.1.0 with no pgmiration tabl
     var database;
     beforeEach(function (done) {
         massive.connect({connectionString: conn, scripts: __dirname + '/db'}, function (err, db) {
-            db.droptesttables(function(err) {
+            db.droptesttables(function (err) {
                 database = db;
                 done();
             })
@@ -23,17 +23,18 @@ describe('When running migration from no tables to 0.1.0 with no pgmiration tabl
 
         var options = {
             connectionString: conn,
-            migrationsDirectory: migrationsDirectory,
-            version: version
+            migrationsDirectory: migrationsDirectory
         };
 
-        semverMigration.up(options, function() {
-            massive.connect(database, function(err, db) {
-                db.tables.length.should.equal(3);
-                db.tables[0].name.should.equal('customer');
-                db.tables[1].name.should.equal('pgmigration');
-                db.tables[2].name.should.equal('salutation');
-                done();
+        massiveMigrateSemVer(options, function (err, semVerMigrations) {
+            semVerMigrations.up({version: version}, function (err) {
+                massive.connect(database, function (err, db) {
+                    db.tables.length.should.equal(3);
+                    db.tables[0].name.should.equal('customer');
+                    db.tables[1].name.should.equal('pgmigration');
+                    db.tables[2].name.should.equal('salutation');
+                    done();
+                })
             })
         });
     })
@@ -44,7 +45,7 @@ describe('When running migration from no tables to 0.2.0 with no pgmigration tab
     var database;
     beforeEach(function (done) {
         massive.connect({connectionString: conn, scripts: __dirname + '/db'}, function (err, db) {
-            db.droptesttables(function(err) {
+            db.droptesttables(function (err) {
                 database = db;
                 done();
             })
@@ -58,17 +59,18 @@ describe('When running migration from no tables to 0.2.0 with no pgmigration tab
 
         var options = {
             connectionString: conn,
-            migrationsDirectory: migrationsDirectory,
-            version: version
+            migrationsDirectory: migrationsDirectory
         };
 
-        semverMigration.up(options, function() {
-            massive.connect(database, function(err, db) {
-                db.tables.length.should.equal(3);
-                db.tables[0].name.should.equal('customer');
-                db.tables[1].name.should.equal('pgmigration');
-                db.tables[2].name.should.equal('salutation');
-                done();
+        massiveMigrateSemVer(options, function (err, semVerMigrations) {
+            semVerMigrations.up({version: version}, function (err) {
+                massive.connect(database, function (err, db) {
+                    db.tables.length.should.equal(3);
+                    db.tables[0].name.should.equal('customer');
+                    db.tables[1].name.should.equal('pgmigration');
+                    db.tables[2].name.should.equal('salutation');
+                    done();
+                })
             })
         });
     })
@@ -80,7 +82,7 @@ describe('When running migration from existing pgmigration table from 0.1.0 to 0
     var database;
     beforeEach(function (done) {
         massive.connect({connectionString: conn, scripts: __dirname + '/db'}, function (err, db) {
-            db.droptesttables(function(err) {
+            db.droptesttables(function (err) {
                 database = db;
                 done();
             })
@@ -93,30 +95,25 @@ describe('When running migration from existing pgmigration table from 0.1.0 to 0
         var version0_1_0 = '0.1.0';
         var version0_2_0 = '0.2.0';
 
-        var options0_1_0_up = {
+        var options = {
             connectionString: conn,
-            migrationsDirectory: migrationsDirectory,
-            version: version0_1_0
+            migrationsDirectory: migrationsDirectory
         };
 
-        var options0_2_0_up = {
-            connectionString: conn,
-            migrationsDirectory: migrationsDirectory,
-            version: version0_2_0
-        };
-
-        semverMigration.up(options0_1_0_up, function() {
-            semverMigration.up(options0_2_0_up, function() {
-                massive.connect(database, function(err, db) {
-                    db.tables.length.should.equal(3);
-                    db.tables[0].name.should.equal('customer');
-                    db.tables[1].name.should.equal('pgmigration');
-                    db.tables[2].name.should.equal('salutation');
-                    db.pgmigration.findOne({ name: '0.2.0'}, function(err, result) {
-                        should.exist(result);
-                        result.name.should.equal('0.2.0');
-                        done();
-                    });
+        massiveMigrateSemVer(options, function (err, semVerMigrations) {
+            semVerMigrations.up({ version : version0_1_0}, function (err) {
+                semVerMigrations.up({ version: version0_2_0}, function(err) {
+                    massive.connect(database, function (err, db) {
+                        db.tables.length.should.equal(3);
+                        db.tables[0].name.should.equal('customer');
+                        db.tables[1].name.should.equal('pgmigration');
+                        db.tables[2].name.should.equal('salutation');
+                        db.pgmigration.findOne({name: '0.2.0'}, function (err, result) {
+                            should.exist(result);
+                            result.name.should.equal('0.2.0');
+                            done();
+                        });
+                    })
                 })
             })
         });
@@ -128,7 +125,7 @@ describe('When running migration from existing pgmigration table from 0.1.0 to 0
     var database;
     beforeEach(function (done) {
         massive.connect({connectionString: conn, scripts: __dirname + '/db'}, function (err, db) {
-            db.droptesttables(function(err) {
+            db.droptesttables(function (err) {
                 database = db;
                 done();
             })
@@ -141,129 +138,14 @@ describe('When running migration from existing pgmigration table from 0.1.0 to 0
         var version0_1_0 = '0.1.0';
         var version0_3_0 = '0.3.0';
 
-        var options0_1_0_up = {
+        var options = {
             connectionString: conn,
-            migrationsDirectory: migrationsDirectory,
-            version: version0_1_0
+            migrationsDirectory: migrationsDirectory
         };
 
-        var options0_3_0_up = {
-            connectionString: conn,
-            migrationsDirectory: migrationsDirectory,
-            version: version0_3_0
-        };
-
-        semverMigration.up(options0_1_0_up, function() {
-            semverMigration.up(options0_3_0_up, function() {
-                massive.connect(database, function(err, db) {
-                    db.tables.length.should.equal(4);
-                    db.tables[0].name.should.equal('customer');
-                    db.tables[1].name.should.equal('pgmigration');
-                    db.tables[2].name.should.equal('salutation');
-                    db.pgmigration.findOne({ name: '0.3.0'}, function(err, result) {
-                        should.exist(result);
-                        result.name.should.equal('0.3.0');
-                        db.pgmigration.find({}, function(err, allresult) {
-                            allresult.length.should.equal(3);
-                            done();
-                        });
-                    });
-                })
-            })
-        });
-    })
-});
-
-describe('When running migration from existing pgmigration table from 0.2.0 to 0.3.0', function () {
-
-    var database;
-    beforeEach(function (done) {
-        massive.connect({connectionString: conn, scripts: __dirname + '/db'}, function (err, db) {
-            db.droptesttables(function(err) {
-                database = db;
-                done();
-            })
-        });
-    });
-
-    it('should contain 4 tables', function (done) {
-        var conn = "postgresql://postgres:postgres@localhost:5432/postgres";
-        var migrationsDirectory = __dirname + "/migrations/none-0.2.0-0.3.0";
-        var version0_2_0 = '0.2.0';
-        var version0_3_0 = '0.3.0';
-
-        var options0_1_0_up = {
-            connectionString: conn,
-            migrationsDirectory: migrationsDirectory,
-            version: version0_2_0
-        };
-
-        var options0_3_0_up = {
-            connectionString: conn,
-            migrationsDirectory: migrationsDirectory,
-            version: version0_3_0
-        };
-
-        semverMigration.up(options0_1_0_up, function() {
-            semverMigration.up(options0_3_0_up, function() {
-                massive.connect(database, function(err, db) {
-                    db.tables.length.should.equal(4);
-                    db.tables[0].name.should.equal('customer');
-                    db.tables[1].name.should.equal('pgmigration');
-                    db.tables[2].name.should.equal('salutation');
-                    db.pgmigration.findOne({ name: '0.3.0'}, function(err, result) {
-                        should.exist(result);
-                        result.name.should.equal('0.3.0');
-                        db.pgmigration.find({}, function(err, allresult) {
-                            allresult.length.should.equal(3);
-                            done();
-                        });
-                    });
-                })
-            })
-        });
-    })
-});
-
-describe('When running migration from existing pgmigration table from 0.1.0 through 0.2.0 to 0.3.0', function () {
-
-    var database;
-    beforeEach(function (done) {
-        massive.connect({connectionString: conn, scripts: __dirname + '/db'}, function (err, db) {
-            db.droptesttables(function(err) {
-                database = db;
-                done();
-            })
-        });
-    });
-
-    it('should contain 4 tables', function (done) {
-        var conn = "postgresql://postgres:postgres@localhost:5432/postgres";
-        var migrationsDirectory = __dirname + "/migrations/none-0.2.0-0.3.0";
-        var version0_1_0 = '0.1.0';
-        var version0_2_0 = '0.2.0';
-        var version0_3_0 = '0.3.0';
-
-        var options0_1_0_up = {
-            connectionString: conn,
-            migrationsDirectory: migrationsDirectory,
-            version: version0_1_0
-        };
-
-        var options0_2_0_up = {
-            connectionString: conn,
-            migrationsDirectory: migrationsDirectory,
-            version: version0_2_0
-        };
-
-        var options0_3_0_up = {
-            connectionString: conn,
-            migrationsDirectory: migrationsDirectory,
-            version: version0_3_0
-        };
-        semverMigration.up(options0_1_0_up, function () {
-            semverMigration.up(options0_2_0_up, function () {
-                semverMigration.up(options0_3_0_up, function () {
+        massiveMigrateSemVer(options, function (err, semVerMigrations) {
+            semVerMigrations.up({ version: version0_1_0}, function (err) {
+                semVerMigrations.up( {version : version0_3_0}, function(err) {
                     massive.connect(database, function (err, db) {
                         db.tables.length.should.equal(4);
                         db.tables[0].name.should.equal('customer');
@@ -277,6 +159,102 @@ describe('When running migration from existing pgmigration table from 0.1.0 thro
                                 done();
                             });
                         });
+                    })
+                })
+            })
+        });
+    })
+});
+
+describe('When running migration from existing pgmigration table from 0.2.0 to 0.3.0', function () {
+
+    var database;
+    beforeEach(function (done) {
+        massive.connect({connectionString: conn, scripts: __dirname + '/db'}, function (err, db) {
+            db.droptesttables(function (err) {
+                database = db;
+                done();
+            })
+        });
+    });
+
+    it('should contain 4 tables', function (done) {
+        var conn = "postgresql://postgres:postgres@localhost:5432/postgres";
+        var migrationsDirectory = __dirname + "/migrations/none-0.2.0-0.3.0";
+        var version0_2_0 = '0.2.0';
+        var version0_3_0 = '0.3.0';
+
+        var options = {
+            connectionString: conn,
+            migrationsDirectory: migrationsDirectory
+        };
+
+
+        massiveMigrateSemVer(options, function (err, semVerMigrations) {
+            semVerMigrations.up({ version: version0_2_0}, function (err) {
+                semVerMigrations.up({ version: version0_3_0}, function(err) {
+                    massive.connect(database, function (err, db) {
+                        db.tables.length.should.equal(4);
+                        db.tables[0].name.should.equal('customer');
+                        db.tables[1].name.should.equal('pgmigration');
+                        db.tables[2].name.should.equal('salutation');
+                        db.pgmigration.findOne({name: '0.3.0'}, function (err, result) {
+                            should.exist(result);
+                            result.name.should.equal('0.3.0');
+                            db.pgmigration.find({}, function (err, allresult) {
+                                allresult.length.should.equal(3);
+                                done();
+                            });
+                        });
+                    })
+                })
+            })
+        });
+    })
+});
+
+describe('When running migration from existing pgmigration table from 0.1.0 through 0.2.0 to 0.3.0', function () {
+
+    var database;
+    beforeEach(function (done) {
+        massive.connect({connectionString: conn, scripts: __dirname + '/db'}, function (err, db) {
+            db.droptesttables(function (err) {
+                database = db;
+                done();
+            })
+        });
+    });
+
+    it('should contain 4 tables', function (done) {
+        var conn = "postgresql://postgres:postgres@localhost:5432/postgres";
+        var migrationsDirectory = __dirname + "/migrations/none-0.2.0-0.3.0";
+        var version0_1_0 = '0.1.0';
+        var version0_2_0 = '0.2.0';
+        var version0_3_0 = '0.3.0';
+
+        var options = {
+            connectionString: conn,
+            migrationsDirectory: migrationsDirectory
+        };
+
+        massiveMigrateSemVer(options, function (err, semVerMigrations) {
+            semVerMigrations.up({ version : version0_1_0}, function (err) {
+                semVerMigrations.up({ version: version0_2_0}, function (err) {
+                    semVerMigrations.up({ version: version0_3_0}, function(err) {
+                        massive.connect(database, function (err, db) {
+                            db.tables.length.should.equal(4);
+                            db.tables[0].name.should.equal('customer');
+                            db.tables[1].name.should.equal('pgmigration');
+                            db.tables[2].name.should.equal('salutation');
+                            db.pgmigration.findOne({name: '0.3.0'}, function (err, result) {
+                                should.exist(result);
+                                result.name.should.equal('0.3.0');
+                                db.pgmigration.find({}, function (err, allresult) {
+                                    allresult.length.should.equal(3);
+                                    done();
+                                });
+                            });
+                        })
                     })
                 })
             });
